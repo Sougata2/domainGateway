6f26a0d3109dac87ab9be9a0fec5420dd003b888
sougata <sougata19582001@gmail.com>
2025-11-23 21:14:26 +0530

    added WebClientBuilder to fetch verify the user and inject role in the request downstream.

--- src/main/java/com/domain/gateway/filter/JwtFilter.java
+++ src/main/java/com/domain/gateway/filter/JwtFilter.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -19,11 +21,11 @@ import java.util.Objects;
 public class JwtFilter implements GlobalFilter, Ordered {
     private final JwtService jwtService;
     private final Environment environment;
+    private final WebClient.Builder builder;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         try {
-            System.out.println("JwtFilter");
             ServerHttpRequest request = exchange.getRequest();
             if (Objects.equals(environment.getProperty("env"), "dev")) {
                 ServerHttpRequest mutatedRequest = request.mutate().header("X-Username", "sougata").header("X-role", "ROLE_ADMIN").build();
@@ -47,10 +49,30 @@ public class JwtFilter implements GlobalFilter, Ordered {
 
             String token = authHeader.substring(7);
             String username = jwtService.validateToken(token);
-            ServerHttpRequest mutatedRequest = request.mutate().header("X-Username", username).build();
-            ServerWebExchange mutatedExchange = exchange.mutate().request(mutatedRequest).build();
 
-            return chain.filter(mutatedExchange);
+
+            return builder.build()
+                    .get()
+                    .uri("lb://auth-service/user/get-default-role/%s".formatted(username))
+                    .retrieve()
+                    .onStatus(HttpStatusCode::isError,
+                            clientResponse -> Mono.error(new RuntimeException("Invalid username in token")))
+                    .bodyToMono(String.class)
+                    .flatMap(response -> {
+
+                        ServerHttpRequest mutatedRequest = request
+                                .mutate()
+                                .header("X-username", username)
+                                .header("X-role", response)
+                                .build();
+
+                        ServerWebExchange mutatedExchange = exchange
+                                .mutate()
+                                .request(mutatedRequest)
+                                .build();
+
+                        return chain.filter(mutatedExchange);
+                    });
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
