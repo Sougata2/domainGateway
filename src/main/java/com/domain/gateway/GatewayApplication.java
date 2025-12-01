75839777dc2d94299d26f74bd18d91d6f84b5149
sougata <sougatadas@cdac.in>
2025-12-01 14:26:41 +0530

    removed the scanBasePackages declaration as it is no longer required.

--- src/main/java/com/domain/gateway/GatewayApplication.java
+++ src/main/java/com/domain/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.domain.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.domain.gateway"})
+@SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
