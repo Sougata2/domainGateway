ca054560b727fa85ed1ccd106e4c0618e2b9fc9a
sougata <sougatadas@cdac.in>
2025-12-02 11:52:54 +0530

    added authentication exception

---
+++ src/main/java/com/domain/gateway/exception/classes/AuthenticationException.java
@@ -0,0 +1,15 @@
+package com.domain.gateway.exception.classes;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(Throwable cause) {
+        super(cause);
+    }
+}
