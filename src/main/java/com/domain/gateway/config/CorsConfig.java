48333c4b63b5a94e085ba2c0819eb9a7bc2ba3fe
sougata <sougata19582001@gmail.com>
2026-01-09 23:58:13 +0530

    added origin pattern to allow backend to be access over network

--- src/main/java/com/domain/gateway/config/CorsConfig.java
+++ src/main/java/com/domain/gateway/config/CorsConfig.java
@@ -17,7 +17,15 @@ public class CorsConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        corsConfiguration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
+                "http://*.local:*",
+                "http://192.168.*.*:*",
+                "http://10.*.*.*:*"
+        ));
+
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION));
         corsConfiguration.setAllowedHeaders(List.of("*"));
